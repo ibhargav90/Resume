1771355ab97dd6fd022315bea0a1f74939d7020c
ibhargav90 <ibhargav90@gmail.com>
2025-01-07 11:17:55 +0530

    New site data - intial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
-import image from '@astrojs/image';
-import mdx from '@astrojs/mdx';
-import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://ibhargav90.github.io',
-	base: '/Resume',
-	integrations: [tailwind(), mdx(), image()],
+  integrations: [tailwind()],
 });
